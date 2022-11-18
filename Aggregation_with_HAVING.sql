0d3cfaa0a4038df24ca9c32b3d432e72c0f23e27
Ryan Wall <rjwall2@students.cs.ubc.ca>
2022-11-17 18:42:23 -0800

    added comments

--- Aggregation_with_HAVING.sql
+++ Aggregation_with_HAVING.sql
@@ -1,3 +1,6 @@
+--Return bands that have made over a specified amount of money from total concert revenue
+--along with the total sum of concert revenue 
+
 SELECT BandPlayed, SUM(ConcertRevenue)
 FROM Past_Concerts_1 pc1, Past_Concerts_2 pc2
 WHERE pc1.TicketsSold = pc2.TicketsSold and pc1.PricePerTicket = pc2.PricePerTicket

5bdcb34f08cd7dfde1eae02327085fa880f2bea3
trixiec <trixiec@students.cs.ubc.ca>
2022-11-18 18:58:37 -0800

    edit button redirect to new php window

--- toy.php
+++ toy.php
@@ -33,21 +33,10 @@
         <hr />
 
         <h2 style ="color:green"> Edit Band Information </h2>
-        <form method = "POST" action ="toy.php">
+        <form method = "POST" action ="edit-window.php" target="_blank">
             <input type = "hidden" id = "editBand" name ="editBand">
             Name of Band to Edit: <input type ="text" name = "editedBand">
             <input type="submit" value = "Edit" name = "Edit">
-        </form>
-
-        <form method = "POST" action ="toy.php">
-            <input type = "hidden" id = "updateQuery" name ="updateQuery">
-            Old Band Name: <input type="text" name="oldName"> <br /><br />
-            New Band Name: <input type="text" name="newName"> <br /><br />
-            Old Charts Rating: <input type="text" name="oldRating"> <br /><br />
-            New Charts Rating: <input type="text" name="newRating"> <br /><br />
-            Old Record Label Name: <input type="text" name="oldLabel"> <br /><br />
-            New Record Label Name: <input type="text" name="newLabel"> <br /><br />
-            <input type="submit" value = "Apply Changes" name = "Apply Changes">
         </form>
 
         <hr />

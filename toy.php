53e2300b0da9048eb51c0aceb6f04163835520a1
Ryan Wall <rjwall2@students.cs.ubc.ca>
2022-11-18 18:24:43 -0800

    added login html and some php for database connection and sql interpretation

--- toy.php
+++ toy.php
@@ -4,6 +4,15 @@
     </head>
     
     <body>
+
+    <h2 style ="color:green"> Please Login to DataBase </h2>
+        <form method = "POST" action = "toy.php">
+            <input type = "hidden" id="login" name = "login">
+            <label> Username: </label> <input type = "text" value ="ora_cwl" name = "username"><br />
+            <label> Password:  </label> <input type = "password" name = "password">
+            <input type = "submit" value = "login" name = "login submit">
+        </form>
+
         <h2 style ="color:red"> Add a New Band! </h2>
         <!-- <p style="color:blue; font-size:15px;"> Add a band by specifying their name </p> -->
         <form method = "POST" action = "toy.php">
@@ -74,6 +83,102 @@
             <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
             <input type="submit" value="suprise" id ="suprise" name="suprise">
         </form>
+
+        <?php
+        // now doing php
+        $db_connect_identifier = NULL; //database connection identifier, or false
+        $show_alert_messages = TRUE; //change to false if don't want to show error messages
+
+        // creates an error message of whatever the parameter is
+        function alert_message($message){
+            global $show_alert_messages;
+
+            if($show_alert_messages){
+                echo "<script type='text/javascript'>alert('" . $message . "');</script>";
+            }
+        }
+
+        //handles connecting to database using the login info, assigns database connection identifier code to global variable
+        function connect_to_database (){
+            global $db_connect_identifier;
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $db_connect_identifier = OCIlogon($username, $password, "dbhost.students.cs.ubc.ca:1522/stu");
+
+            if ($db_connect_identifier) {
+                alert_messages("Connected to database successfully!");
+        
+            }
+            else{
+                alert_messages("Could not connect, try re-entering information");
+                $error = OCI_Error(); // creates error object that contains information on the last error, in this case error from login failure
+                echo htmlentities($error['message']); //converts characters in message to html entities
+            
+            }
+        }
+
+        //runs plain sql statements inputted
+        function runPlainSQL($SQLcommand){
+            global $db_connect_identifier;
+
+            $SQLcommandparsed = OCIParse($db_connect_identifier, $SQLcommand); //parses the SQL command inputted
+
+            //checks if SQL command was parsed successfullly 
+            if (!$SQLcommandparsed) {
+                echo "<br>Cannot parse the following command: " . $SQLcommand . "<br>";
+                $error = OCI_Error($db_connect_identifier); // For OCIParse errors pass the connection handle
+                echo htmlentities($error['message']);
+            }
+
+            $SQLexecution = OCIExecute($SQLcommandparsed, OCI_DEFAULT); //executes the parsed SQL command
+
+            //checks if parsed SQL command executed successfully
+            if (!$SQLexecution) {
+                echo "<br>Cannot execute the following command: " . $SQLcommand . "<br>";
+                $error = oci_error($SQLcommandparsed); // For OCIExecute errors pass the statementhandle
+                echo htmlentities($error['message']);
+            }
+
+            return $SQLcommandparsed;
+        }
+
+        //runs bound sql statements, use for adding tuples
+        function executeBoundSQL($SQLcommand, $list) {
+
+			global $db_connect_identifier;
+
+            $SQLcommandparsed = OCIParse($db_connect_identifier, $SQLcommand); //parses the SQL command inputted
+
+            //checks if SQL command was parsed successfullly 
+            if (!$SQLcommandparsed) {
+                echo "<br>Cannot parse the following command: " . $SQLcommand . "<br>";
+                $error = OCI_Error($db_connect_identifier); // For OCIParse errors pass the connection handle
+                echo htmlentities($error['message']);
+            }
+
+            foreach ($list as $tuple) {
+                foreach ($tuple as $bind => $val) {
+                    //echo $val;
+                    //echo "<br>".$bind."<br>";
+                    OCIBindByName($SQLcommandparsed, $bind, $val);
+                    unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+				}
+
+                $SQLexecution = OCIExecute($SQLcommandparsed, OCI_DEFAULT); //executes the parsed SQL command
+
+                //checks if parsed SQL command executed successfully
+                if (!$SQLexecution) {
+                    echo "<br>Cannot execute the following command: " . $SQLcommand . "<br>";
+                    $error = OCI_ERROR($SQLcommandparsed); // For OCIExecute errors pass the statementhandle
+                    echo htmlentities($error['message']);
+                    echo "<br>";
+                }
+            }
+        }
+
+        ?>
     </body>
 </html>
 
